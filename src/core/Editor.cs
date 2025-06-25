b2323d3419c9d9b9b8dfe247f4df0df189ab7842
Sjoerd Wouters <sjoerdwouters2004@gmail.com>
2025-06-25 23:05:15 +0200

    console window basics

--- src/core/Editor.cs
+++ src/core/Editor.cs
@@ -48,6 +48,8 @@ public static unsafe class Editor
 
     private static string fileDialogPath = "";
 
+    private static string consoleSearchPrompt = "";
+
     public static void Update(float deltaTime)
     {
         if (sceneWindowFocussed) sceneCamera.ApplyMovement(deltaTime);
@@ -375,13 +377,34 @@ public static unsafe class Editor
 
         ImGui.End();
 
-        ImGui.Begin("Console");
+        ImGui.Begin("Console", ImGuiWindowFlags.NoScrollbar);
 
-        if (ImGui.Button("test")) Debug.Log("test");
-        if (ImGui.Button("clear")) Debug.Clear();
+        var consoleButtonWidth = 80;
+        var consoleButtonWithSpacingWidth = consoleButtonWidth + ImGui.GetStyle().ItemSpacing.X;
+        var searchBoxWidth = ImGui.GetContentRegionAvail().X - (consoleButtonWithSpacingWidth * 2f);
 
-        List<string> history = Debug.history;
-        foreach (var line in history) ImGui.Text(line);
+        ImGui.SetNextItemWidth(searchBoxWidth);
+        ImGui.InputText("##search", ref consoleSearchPrompt, 100);
+        ImGui.SameLine();
+        if (ImGui.Button("clear", new(consoleButtonWidth, 0))) consoleSearchPrompt = "";
+        ImGui.SameLine();
+        if (ImGui.Button("reset", new(consoleButtonWidth, 0))) Debug.Clear();
+
+        //if (ImGui.Button("test", new(consoleButtonWidth, 0))) Debug.Log("asdfasdfasdf_" + new Random().Next(88));
+
+        ImGui.Separator();
+
+        ImGui.BeginChild("ConsoleHistoryScrollRegion", ImGui.GetContentRegionAvail());
+        var history = Debug.history;
+        
+        for (int index = 0; index < history.Count; index++)
+        {
+            string line = history[index];
+            if (line.ToLower().Contains(consoleSearchPrompt.ToLower())) if (ImGui.Selectable(line)) ImGui.SetClipboardText(line);
+        }
+
+        // ImGui.SetScrollY(ImGui.GetScrollMaxY());
+        ImGui.EndChild();
 
         ImGui.End();
 
